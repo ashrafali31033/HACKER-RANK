ee18d480582cfed633690d46b9fc13770df21d18
S MOHAMMAD ASHRAF <ashrafali31033@gmail.com>
2025-01-06 15:14:59 +0530

    Create if-elseif.java

---
+++ if-elseif.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class ashraf
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        
+        int N=sc.nextInt();
+        
+        if(N%2!=0)
+        {
+            System.out.println("Weird");
+            
+            
+        }
+        else if(N%2==0 && N>=2 && N<=5)
+        {
+            System.out.println("Not Weird");
+        }
+        else if(N%2==0 && N>=6 && N<=20)
+        {
+            System.out.println("Weird");
+        }
+        else if(N%2==0 && N>20)
+        {
+            System.out.println("Not Weird");
+        }
+    }
+}
